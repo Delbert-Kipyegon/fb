0a5e0488c9113c9b033e962a59a40d417394ceb9
Delbert Kipyegon <delbertkip@gmail.com>
2023-05-25 12:34:42 +0300

    3rd

--- src/components/Login.js
+++ src/components/Login.js
@@ -15,8 +15,6 @@ export default function Login() {
    async function handleSubmit(e) {
         e.preventDefault()
         
-       
-
         // signs us up 
         try {
             setError('')
